9b907bfe2bb7d32928f5c3dbfdb852459a6176c4
Emmy VILLARD <emmy.villard.pro@proton.me>
2025-12-08 19:09:20 +0100

    feat(js): add some more const for new form elements in createRecipe.js

--- frontend/assets/js/createRecipe.js
+++ frontend/assets/js/createRecipe.js
@@ -7,15 +7,16 @@ function createRecipe(event) {
     const recipeDescription = document.getElementById("new-description");
     const recipeIngredients = document.getElementById("new-ingredients");
     const recipeInstructions = document.getElementById("new-instructions");
-    const recipeDietType = document.getElementById("new-diet-type");
+    const recipeDietType = document.querySelector('input[name="diet-type"]:checked');
     const recipePrepTime = document.getElementById("new-prep-time");
     const recipeCookTime = document.getElementById("new-cook-time");
-    const recipeImgUrl = document.getElementById("new-image");
     const recipeServings = document.getElementById("new-servings");
     const recipeKcalPerServing = document.getElementById("new-kcal-per-serving");
     const recipeDifficulty = document.querySelector('input[name="difficulty"]:checked');
     const last_modified = new Date().toISOString();
     const slug = generateSlug(recipeTitle.value);
+    const recipeImgUrl = document.getElementById("new-img-url");
+    const recipeImg = document.getElementById("new-img");
 
     // Rest API call to create the recipe would go here
     // Need : title, description, ingredients, instructions, diet type, time, image URL
