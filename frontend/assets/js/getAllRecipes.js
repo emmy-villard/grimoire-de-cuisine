5dc808bafea88c144aeee31f3d5eb6cc570570e5
Emmy VILLARD <emmy.villard.pro@proton.me>
2025-12-16 13:43:48 +0100

    feat(js): add backup recipes if no recipes are found

--- frontend/assets/js/getAllRecipes.js
+++ frontend/assets/js/getAllRecipes.js
@@ -1,4 +1,4 @@
-async function getAllRecipes() {
+async function getAllRecipes(backup=true) {
     const recipes = [];
     let id = 0;
     let recipeJSON = localStorage.getItem(`recipe${id}`);
@@ -12,10 +12,20 @@ async function getAllRecipes() {
         id += 1;
         recipeJSON = localStorage.getItem(`recipe${id}`);
     }
+
+    if (recipes.length == 0 && backup) {
+        return jsonBackupRecipes();
+    }
     return recipes;
 }
 
 async function getNextId() {
-  const recipes = await getAllRecipes();
+  const recipes = await getAllRecipes(false);
   return recipes.length;
+}
+
+async function jsonBackupRecipes() {
+    const response = await fetch("assets/json/allRecipes.json");
+    const json = await response.json();
+    return json;
 }
