6b740b49626e8ef8525afb96109191398811d703
Emmy VILLARD <emmy.villard.pro@proton.me>
2025-12-16 13:23:23 +0100

    feat(js): add showRecipe.js shows all recipes (getAllRecipes) in index.html

---
+++ frontend/assets/js/showRecipes.js
@@ -0,0 +1,33 @@
+async function showRecipe() {
+    const recipes = await getAllRecipes();
+    console.log(recipes);
+    // Retrieve the DOM element that will host the recipes
+    const recipesContainer = document.getElementById("recipes-container");
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        const recipeElement = document.createElement("article");
+        recipeElement.dataset.id = recipe.id;
+
+        // Create element
+        const recipeImg = document.createElement("img");
+        recipeImg.src = recipe.image_url;
+        recipeImg.alt = recipe.title;
+        const recipeTitle = document.createElement("h2");
+        recipeTitle.innerText = recipe.title;
+        const recipeDesc = document.createElement("p");
+        recipeDesc.innerText = recipe.description;
+        const recipeDietType = document.createElement("p");
+        recipeDietType.innerText = "Régime : " + recipe.diet_type;
+        const recipePrepCookTime = document.createElement("p");
+        recipePrepCookTime.innerText = `Durée de préparation : ${recipe.prepTime} min // Durée de cuisson :  ${recipe.cookTime} min `;
+
+        // Add elements to the DOM
+        recipeElement.appendChild(recipeImg);
+        recipeElement.appendChild(recipeTitle);
+        recipeElement.appendChild(recipeDesc);
+        recipeElement.appendChild(recipeDietType);
+        recipeElement.appendChild(recipePrepCookTime);
+    }
+}
+
+showRecipe();
