65d438896325d0d81a95c604e18650690b19ec0d
Emmy VILLARD <emmy.villard.pro@proton.me>
2025-12-17 17:26:50 +0100

    chore(js): add minimal test and moved test folder

---
+++ frontend/tests/unit/editRecipe.test.js
@@ -0,0 +1,1 @@
+it('sanity', () => expect(1+1).toBe(2));
--- tests/unit/createRecipe.test.js
+++
@@ -1,7 +0,0 @@
-import { add } from './math.js';
-
-describe('add', () => {
-  it('additionne deux nombres', () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
