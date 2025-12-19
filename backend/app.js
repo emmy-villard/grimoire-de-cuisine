302a9c0727d8eeadbe1c73a13601bbce6a15e5c5
Emmy <emmy.villard.pro@proton.me>
2025-12-19 20:34:37 +0100

    chore(js): remove usless return

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ app.use('/recipes/:slug', rootHandler);
 function rootHandler(req, res, next) {
     res.status(200);
     res.json( {message: `Requête reçue : ${req.params.slug}`} );
-    return;
 }
 
 export default app;
