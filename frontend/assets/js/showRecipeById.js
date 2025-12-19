c0c11a1186dd1cba595b9f122a19dba3cb3d61c8
Emmy <emmy.villard.pro@proton.me>
2025-12-19 21:28:56 +0100

    feat(js): send GET 'recipe/:id' request to API

--- frontend/assets/js/showRecipeById.js
+++ frontend/assets/js/showRecipeById.js
@@ -1,8 +1,14 @@
+import getApiUrl from "./getApiUrl.js";
+
 async function showRecipeById() {
     const params = new URLSearchParams(document.location.search);
     const recipeId = params.get("id");
+    const api_url = getApiUrl();
     console.log(recipeId);
-    //const res = await fetch('/api/recipes/' + event.target.dataset.id);
+    const res = await fetch(`${api_url}/recipes/${recipeId}`, {
+        method: "GET"
+    });
+    console.log(res);
     //console.log("Recipe added : " + recipeTitle + " with id : " + id);
 }
 
