7c4ed3140c69c9bf94ebd54436b7324f8fc5421b
Emmy <emmy.villard.pro@proton.me>
2025-12-19 21:34:51 +0100

    feat(js): move allowAllOrigins before handleRecipeById

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 const app = express();
+app.use('/recipes/:id', allowAllOrigins);
 app.use('/recipes/:id', handleRecipeById);
 
-app.use((req, res, next) => {
+function allowAllOrigins(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+}
 
 function handleRecipeById(req, res, next) {
     res.status(200);
