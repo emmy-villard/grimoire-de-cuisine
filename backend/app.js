763bf9839de39f154d3f363bed1d3484774fccf0
Emmy <emmy.villard.pro@proton.me>
2025-12-20 18:18:42 +0100

    feat(js): add all routes in app.js : get recipes, post recipes, put recipes/id, delete recipes/id

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,9 @@ import allowAllOrigins from './allowAllOrigins';
 const app = express();
 app.use('*', allowAllOrigins);
 app.get('/recipes/:id', getRecipeById);
+app.get('/recipes', getAllRecipes);
+app.post('/recipes', postRecipe);
+app.put('/recipes/:id', editRecipe);
+app.delete('/recipes/:id', deleteRecipe);
 
 export default app;
