8672ceeca676101e0bb645b6220b03883c193fc0
Emmy <emmy.villard.pro@proton.me>
2025-12-20 18:14:01 +0100

    refactor(js): move allowAllOrigins to its own file

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,9 @@
 import express from 'express';
 import getRecipeById from './controllers/getRecipeById';
+import allowAllOrigins from './allowAllOrigins';
 
 const app = express();
 app.use('*', allowAllOrigins);
 app.get('/recipes/:id', getRecipeById);
-
-function allowAllOrigins(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-}
 
 export default app;
