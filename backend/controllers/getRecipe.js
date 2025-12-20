a9ce02e66dabaa0a236159c8abbd115cf7c364ca
Emmy <emmy.villard.pro@proton.me>
2025-12-20 18:33:11 +0100

    refactor(js): rename getRecipeById to getRecipe

--- backend/controllers/getRecipe.js
+++ backend/controllers/getRecipe.js
@@ -1,6 +1,6 @@
-function getRecipeById(req, res, next) {
+function getRecipe(req, res, next) {
     res.status(200);
     res.json( {message: `Requête reçue : ${req.params.id}`} );
 }
 
-export default getRecipeById;
+export default getRecipe;
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import getAllRecipes from '../controllers/getAllRecipes.js';
-import getRecipeById from '../controllers/getRecipeById.js';
+import getRecipe from '../controllers/getRecipe.js';
 import postRecipe from '../controllers/postRecipe.js';
 import editRecipe from '../controllers/editRecipe.js';
 import deleteRecipe from '../controllers/deleteRecipe.js';
@@ -8,7 +8,7 @@ import deleteRecipe from '../controllers/deleteRecipe.js';
 const router = express.Router();
 
 router.get('/', getAllRecipes);
-router.get('/:id', getRecipeById);
+router.get('/:id', getRecipe);
 router.post('/', postRecipe);
 router.put('/:id', editRecipe);
 router.delete('/:id', deleteRecipe);
