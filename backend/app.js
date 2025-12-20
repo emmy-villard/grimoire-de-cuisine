785a1a4dc1304a5f2fb7828a4e821e5fd7fdfcf6
Emmy <emmy.villard.pro@proton.me>
2025-12-20 18:23:44 +0100

    feat(js): add imports to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import getRecipeById from './controllers/getRecipeById';
-import allowAllOrigins from './allowAllOrigins';
+import getRecipeById from './controllers/getRecipeById.js';
+import allowAllOrigins from './allowAllOrigins.js';
+import postRecipe from './controllers/postRecipe.js'
+import getAllRecipes from './controllers/getAllRecipes.js';
+import editRecipe from './controllers/editRecipe.js';
+import deleteRecipe from './controllers/deleteRecipe.js'
 
 const app = express();
 app.use('*', allowAllOrigins);
