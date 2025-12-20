019b5c4d3dbaf592ab36a9f2483640c7cb9fe551
Emmy <emmy.villard.pro@proton.me>
2025-12-20 18:36:13 +0100

    chore(js): add temp comments to postRecipe

--- backend/controllers/postRecipe.js
+++ backend/controllers/postRecipe.js
@@ -1,4 +1,20 @@
 function postRecipe(req, res, next) {
+    /*
+    recipeTitle
+    recipeDescription
+    recipeIngredients
+    recipeInstructions
+    recipeDietType
+    recipePrepTime
+    recipeCookTime
+    recipeServings
+    recipeKcalPerServing
+    recipeDifficulty
+    last_modified
+    slug
+    recipeImgUrl
+    (et l’ID on va le créé auto avec SQL, pas ATM)%
+    */
 }
 
 export default postRecipe;
