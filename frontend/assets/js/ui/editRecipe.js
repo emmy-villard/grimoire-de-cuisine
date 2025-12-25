f31a9544f435615dd1e9dbeff1989de4299fdf2b
Emmy <emmy.villard.pro@proton.me>
2025-12-25 18:36:58 +0100

    feat(js): add editRecipe logic

--- frontend/assets/js/ui/editRecipe.js
+++ frontend/assets/js/ui/editRecipe.js
@@ -1,6 +1,17 @@
-function editRecipeLS(event) {
+import getNextId from '../js/getNextId.js';
+import { CONFIG } from '../config/config.js';
+
+async function editRecipe(event) {
     event.preventDefault();
-    return;
+    const recipeJson = formDataToJson();
+    if ( CONFIG.mode == "DEMO" ) {
+        recipeJson[id] = await getNextId();
+        window.localStorage.setItem(`recipe${id}`, recipeJson);
+        console.log("Recipe added in local storage : "
+            + recipeTitle + " with id : " + id);
+    } else {
+        //API Call
+    }
 }
 
-export default editRecipeLS;
+export default editRecipe;
