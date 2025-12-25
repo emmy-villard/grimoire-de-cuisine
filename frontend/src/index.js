e2e00e1ac5c9fdaba1af0e8e4e05d0a39664d7d9
Emmy <emmy.villard.pro@proton.me>
2025-12-25 17:22:15 +0100

    chore(js): utile pathname variable for window.location.pathname in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,15 @@ import showRecipe from '../js/showRecipes.js';
 import editRecipe from './ui/editRecipe.js';
 import showRecipeById from './ui/showRecipeById.js';
 
-if (window.location.pathname === '/' || window.location.pathname === "/index.html") {
+const { pathname } = window.location;
+if (pathname === '/' || pathname === "/index.html") {
     await showRecipe();
-} else if (window.location.pathname === '/new-recipe.html') {
+} else if (pathname === '/new-recipe.html') {
     const newRecipeForm = document.getElementById("new-recipe");
     newRecipeForm.addEventListener("submit", createRecipe);
-} else if (window.location.pathname === '/edit-recipe.html') {
+} else if (pathname === '/edit-recipe.html') {
     let editRecipeForm = document.getElementById("edit-recipe");
     editRecipeForm.addEventListener("submit", editRecipe);
-} else if (window.location.pathname === '/recipe.html') {
+} else if (pathname === '/recipe.html') {
     await showRecipeById();
 }
