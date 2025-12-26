002900bc2b532b86fe274a069d70330750cccae0
Emmy <emmy.villard.pro@proton.me>
2025-12-26 20:19:02 +0100

    feat(js): detect real URLs in DEMO mode (using localStorage)

--- frontend/assets/js/ui/getImgSrc.js
+++ frontend/assets/js/ui/getImgSrc.js
@@ -2,6 +2,9 @@ import { CONFIG } from "../config/config.js";
 
 export default function getImgSrc(url) {
     if (CONFIG.mode == "DEMO") {
+        if(url.startsWith('http')) {
+            return url;
+        }
         const dataImage = localStorage.getItem(url);
         return "data:image/png;base64," + dataImage;
     }
