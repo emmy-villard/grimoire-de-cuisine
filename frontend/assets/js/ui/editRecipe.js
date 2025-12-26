1e193ce69c4b9346477c0f0b16d7a4f45b5f2762
Emmy <emmy.villard.pro@proton.me>
2025-12-26 18:22:39 +0100

    feat(js): add API call for editRecipe.js

--- frontend/assets/js/ui/editRecipe.js
+++ frontend/assets/js/ui/editRecipe.js
@@ -1,17 +1,36 @@
 import getNextId from '../localStorage/getNextIdLS.js';
+import formDataToJson from './formDataToJson.js';
 import { CONFIG } from '../config/config.js';
 
 async function editRecipe(event) {
     event.preventDefault();
+    const params = new URLSearchParams(window.location.search);
+    const recipeId = params.get('id');
     const recipeJson = formDataToJson();
+    const recipe = (typeof recipeJson === 'string') ?
+        JSON.parse(recipeJson) : recipeJson;
+    recipe.id = recipeId;
+    console.log(recipe);
     if ( CONFIG.mode == "DEMO" ) {
-        const recipeId = await getNextId();
-        recipeJson[id] = recipeId;
-        window.localStorage.setItem(`recipe${recipeId}`, recipeJson);
+        window.localStorage.setItem(`recipe${recipeId}`,
+            JSON.stringify(recipeJson));
         console.log("Recipe added in local storage : "
             + recipeTitle + " with id : " + recipeId);
     } else {
-        //API Call
+        try {
+            const res = await fetch(`${api_url}/recipes/${recipeId}`, 
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(recipeJson),
+                });
+            return res;
+        } catch(err) {
+            console.error('deleteRecipe error', err);
+            throw err;
+        }
     }
 }
 
