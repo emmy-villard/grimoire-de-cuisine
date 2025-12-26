670530b6bba192d9aecf799c86a6362be25e3345
Emmy <emmy.villard.pro@proton.me>
2025-12-26 18:23:41 +0100

    feat(js): add API call in createRecipe.js (API)

--- frontend/assets/js/ui/createRecipe.js
+++ frontend/assets/js/ui/createRecipe.js
@@ -11,7 +11,20 @@ async function createRecipe(event) {
         window.localStorage.setItem(`recipe${id}`, JSON.stringify(recipeJson));
         console.log("Recipe added in local storage : " + (recipeJson.title || '') + " with id : " + id);
     } else {
-        //API Call
+        try {
+            const res = await fetch(`${api_url}/recipes/${recipeId}`, 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(recipeJson),
+                });
+            return res;
+        } catch(err) {
+            console.error('deleteRecipe error', err);
+            throw err;
+        }
     }
 }
 
