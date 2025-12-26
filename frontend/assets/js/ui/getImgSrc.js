cb13c3c68a38060d6726560dba74bf514ae7a3e6
Emmy <emmy.villard.pro@proton.me>
2025-12-26 22:29:10 +0100

    fix(js): check if url is empty at start of getImgSrc

--- frontend/assets/js/ui/getImgSrc.js
+++ frontend/assets/js/ui/getImgSrc.js
@@ -1,6 +1,9 @@
 import { CONFIG } from "../config/config.js";
 
 export default function getImgSrc(url) {
+    if (!url) {
+        return '/assets/img/default.webp';  
+    }
     if (CONFIG.mode == "DEMO") {
         if(url.startsWith('http')) {
             return url;
@@ -8,5 +11,5 @@ export default function getImgSrc(url) {
         const dataImage = localStorage.getItem(url);
         return "data:image/png;base64," + dataImage;
     }
-    return url ?? '/public/assets/img/default.webp';
+    return url;
 }
